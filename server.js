32427b6d4923fd557905151a6bb1625dfec53b65
Romane Orlando Robb <mail@romanerobb.com>
2023-01-03 11:05:11 -0500

    Added the about me page.

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('<h1>Welcome to Robb Server 001</h1>')
 })
 
+app.get('/about', (req, res) => {
+  res.send('<h1>My name is Romane Orlando Robb and I created this server.</h1>')
+})
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
